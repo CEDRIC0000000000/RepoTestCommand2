1c5669c5106612d09af88c6587da44206d1d1fb3
Cedric yonta <ecyontay01@monccnb.ca>
2021-11-07 00:47:19 +0100

    first commit

---
+++ fichier.cpp
@@ -0,0 +1,24 @@
+/*
+  Titre      : Titre du programme
+  Auteur     : Yonta Cedric
+  Date       : 07/11/2021
+  Description: Une description du programme
+  Version    : 0.0.1
+*/
+
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{cout <<"hello word" << endl;  
+
+
+  }
+
+
+
+
+
